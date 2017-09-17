79a9f49a3a0d927decdd72a18988c39350cb9c8f
Vitek <Vitek@HP-NTB>
2017-09-17 22:45:27 +0200

    lll

--- Neuron/Program.cs
+++ Neuron/Program.cs
@@ -8,8 +8,53 @@ namespace Neuron
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            int[] input_strenght = new int[] {1,1,1,1};
+            int[] input_value = new int[] {0,0,0,0};
+            bool repeat = true;
+            string input;
+            int neuron_limit = 5;
+
+            while (repeat)
+            {
+                Console.WriteLine("Vstup 1:");
+                input = Console.ReadLine();
+                Int32.TryParse(input, out input_value[0]);
+                Console.Clear();
+
+                Console.WriteLine("Vstup 2:");
+                input = Console.ReadLine();
+                Int32.TryParse(input, out input_value[1]);
+                Console.Clear();
+
+                Console.WriteLine("Vstup 3:");
+                input = Console.ReadLine();
+                Int32.TryParse(input, out input_value[2]);
+                Console.Clear();
+
+                Console.WriteLine("Vstup 4:");
+                input = Console.ReadLine();
+                Int32.TryParse(input, out input_value[3]);
+                Console.Clear();
+
+                if (input_value[0] * input_strenght[0] + input_value[1] * input_strenght[1] + input_value[2] * input_strenght[2] + input_value[3] * input_strenght[3] >= neuron_limit)
+                {
+                    Console.WriteLine("Neuron vystřelil");
+                }
+                else
+                {
+                    Console.WriteLine("Neuron nevystřelil");
+                }
+
+                for (int a = 0; a < 3; a++)
+                {
+                    if (input_value[a] >= 1)
+                    {
+                        input_strenght[a]++;
+                    } 
+                }
+            }
         }
     }
 }
